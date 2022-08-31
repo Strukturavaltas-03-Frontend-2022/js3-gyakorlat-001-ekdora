0c5cadfd63d9a2202c3763f69f30458ad15f9aba
ekdora <dora.egrykiss@gmail.com>
2022-08-31 09:11:54 +0200

    Taks solution

--- 08-other/exercises/02/solution/js/HU.js
+++ 08-other/exercises/02/solution/js/HU.js
@@ -1,1 +1,17 @@
+const HU = {
+    date(date) {
+        return new Intl.DateTimeFormat('hu-HU').format(date);
+    },
+    currency(num) {
+        const huCurrency = new Intl.NumberFormat('hu-HU', {
+            style: 'currency',
+            currency: 'HUF'
+        })
+        return huCurrency.format(num);
+    },
+    list(arr) {
+        return `${arr.slice(0, -1).join(', ')} és ${arr.slice(-1)}`;
+    }
+}
+
 export default HU;
--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,26 @@
+const objectConverter = {
+    arrayToMap(array) {
+        return new Map(array.map((element, index) => [index, element]));
+    },
+    arrayToSet(array) {
+        return new Set(array);
+    },
+    setToMap(set) {
+        return new Map(Array.from(set).map((element, index) => [index, element]));
+    },
+    setToArray(set) {
+        return Array.from(set);
+    },
+    mapToArray(map) {
+        const mapArray = [];
+        map.forEach(value => mapArray.push(value));
+        return mapArray;
+    },
+    mapToSet(map) {
+        const setArray = [];
+        map.forEach(value => setArray.push(value));
+        return new Set(setArray);
+    }
+}
+
 export default objectConverter;
